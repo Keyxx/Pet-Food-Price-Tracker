3edfb5b678864cdecd71321db799de67ae1107d1
NimAdm <sjomin.juri@gmail.com>
2024-11-02 17:58:09 +0200

    Init commit

---
+++ src/main/java/com/pettracker/main/PetFoodTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pettracker.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFoodTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFoodTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pettracker/main/PetFoodTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pettracker.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFoodTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
